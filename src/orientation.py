bb6324d1c69f69d74f54e91de4976aba9d4199cb
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2022-04-15 17:53:54 -0500

    Update orientation.py

--- src/orientation.py
+++ src/orientation.py
@@ -11,29 +11,29 @@ from sensor_msgs import Imu
         
 def callback(Imu_data):
     if __name__ == '__main__':
-    rospy.init_node('check_obstacle')
+            rospy.init_node('check_obstacle')
 
-    listener = tf.TransformListener()
+            listener = tf.TransformListener()
 
-    pose_msg = rospy.Publisher('pose_msg', PoseStamped ,queue_size=5)
+            pose_msg = rospy.Publisher('pose_msg', PoseStamped ,queue_size=5)
 
-    rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        try:
-            (trans,rot) = listener.lookupTransform('/tf', rospy.Time(0))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            continue
+            rate = rospy.Rate(10.0)
+            while not rospy.is_shutdown():
+                try:
+                    (trans,rot) = listener.lookupTransform('/tf', rospy.Time(0))
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    continue
 
-        x = trans[0]
-        y = trans[1]
-        angular = math.atan2(trans[1], trans[0])
-        pose = PoseStamped()
-        pose.position.x = x + #Imu_position_x
-        pose.position.y = y + #Imu_position_y
-        pose.orientation.x = angular
-        pose_msg.publish(pose)
+                x = trans[0]
+                y = trans[1]
+                angular = math.atan2(trans[1], trans[0])
+                pose = PoseStamped()
+                pose.position.x = x + #Imu_position_x
+                pose.position.y = y + #Imu_position_y
+                pose.orientation.x = angular
+                pose_msg.publish(pose)
 
-        rate.sleep()
+                rate.sleep()
 
 
 
