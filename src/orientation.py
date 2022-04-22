9a30143aa2fb9c8b53bbc9d92fe6991c3bd036c2
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2022-04-22 18:50:13 -0500

    Update orientation.py

--- src/orientation.py
+++ src/orientation.py
@@ -4,44 +4,38 @@ import roslib
 import rospy
 import math
 import tf
-from geometry_msgs import PoseStamped
-from sensor_msgs import Imu
+from geometry_msgs.msg import Pose
+
 
 #meters
 collection_bin_offset = 1
 
-initial_rotation = (math.pi/2)-(math.asin(0.46/pos)+#angle of depth cam to qr
+#initial_rotation = (math.pi/2)-(math.asin(0.46/pos)+#angle of depth cam to qr
                                           
 
         
-def callback(Imu_data):
-    if __name__ == '__main__':
-            rospy.init_node('check_obstacle')
+if __name__ == '__main__':
+    rospy.init_node('check_obstacle')
 
-            listener = tf.TransformListener()
+    listener = tf.TransformListener()
 
-            pose_msg = rospy.Publisher('pose_msg', PoseStamped ,queue_size=5)
+    pose_msg = rospy.Publisher('pose_msg', Pose ,queue_size=5)
 
-            rate = rospy.Rate(10.0)
-            while not rospy.is_shutdown():
-                try:
-                    (trans,rot) = listener.lookupTransform('/tf', rospy.Time(0))
-                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                    continue
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        try:
+            (trans,rot) = listener.lookupTransform('camera_depth_frame', '/object_18', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
 
-                x = trans[0]
-                y = trans[1]
-                pos = math.sqrt(trans[0]**2+trans[1]**2)
-                angular = math.atan2(trans[1], trans[0])
-                pose = PoseStamped()
-                pose.position.x = x + #Imu_position_x
-                pose.position.y = y - collection_bin_offset + #Imu_position_y
-                pose.orientation.x = angular + #Imu_rotation_from_y_axis
-                pose_msg.publish(pose)
-
-                rate.sleep()
-
-
-
-sub = rospy.Subscriber('/imu', Imu, callback)
-rospy.spin()
+        x = trans[0]
+        y = trans[1]
+        pos = math.sqrt(trans[0]**2+trans[1]**2)
+        angular = math.atan2(trans[1], trans[0])
+        pose = Pose()
+        pose.position.x = x  #plus Imu_position_x
+        pose.position.y = y - collection_bin_offset  #plus Imu_position_y
+        pose.orientation.x = angular #plus Imu_rotation_from_y_axis
+        pose_msg.publish(pose)
+	print(pos)
+        rate.sleep()
