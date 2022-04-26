4cc865dd66d7d770c26cc12e745a84648e7ced9e
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2022-04-25 22:29:49 -0500

    Update orientation.py

--- src/orientation.py
+++ src/orientation.py
@@ -4,22 +4,22 @@ import roslib
 import rospy
 import math
 import tf
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
 
 
 #meters
-collection_bin_offset = 1
+collection_bin_offset = .31
 
 #initial_rotation = (math.pi/2)-(math.asin(0.46/pos)+#angle of depth cam to qr
                                           
 
         
 if __name__ == '__main__':
-    rospy.init_node('check_obstacle')
+    rospy.init_node('orientation')
 
     listener = tf.TransformListener()
 
-    pose_msg = rospy.Publisher('pose_msg', Pose ,queue_size=5)
+    pose_msg = rospy.Publisher('pose_msg1', PoseStamped ,queue_size=5)
 
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
@@ -30,12 +30,19 @@ if __name__ == '__main__':
 
         x = trans[0]
         y = trans[1]
+	z = trans[2]
         pos = math.sqrt(trans[0]**2+trans[1]**2)
-        angular = math.atan2(trans[1], trans[0])
-        pose = Pose()
-        pose.position.x = x  #plus Imu_position_x
-        pose.position.y = y - collection_bin_offset  #plus Imu_position_y
-        pose.orientation.x = angular #plus Imu_rotation_from_y_axis
+        angular_z = math.atan2(trans[1], trans[0])
+        angular_y = math.atan2(trans[0], trans[2])
+        angular_x = math.atan2(trans[2], trans[1])
+        pose = PoseStamped()
+        pose.pose.position.x = x  #plus Imu_position_x
+        pose.pose.position.y = y - collection_bin_offset  #plus Imu_position_y
+	pose.pose.position.z = z
+        pose.pose.orientation.x = angular_z #plus Imu_rotation_from_y_axis
+        pose.pose.orientation.y = angular_x #plus Imu_rotation_from_y_axis
+        pose.pose.orientation.z = angular_y #plus Imu_rotation_from_y_axis
+	pose.header.stamp = rospy.Time.now()
         pose_msg.publish(pose)
 	print(pos)
         rate.sleep()
