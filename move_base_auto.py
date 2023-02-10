391e507ed61ea5830001bc73d807b04b730edd2f
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2023-02-10 16:54:39 -0600

    Update move_base_auto.py

--- move_base_auto.py
+++ move_base_auto.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # license removed for brevity
 # Largely recycled from https://hotblackrobotics.github.io/en/blog/2018/01/29/action-client-py/
+# https://automaticaddison.com/how-to-send-goals-to-the-ros-navigation-stack-using-c/
 
 import rospy
 
@@ -8,15 +9,22 @@ import rospy
 import actionlib
 # Brings in the .action file and messages used by the move base action
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+# So that we can subscribe to the tf
+from geometry_msgs.msg import PoseStamped
 
-def movebase_client():
+def movebase_client(data):
+    rospy.set_param('nav_to_mine', 0)
 
    # Get the goal wrt map frame (Is origin of map frame the location of the qr code??)
    # if map frame if where robot is initialized, we need to figure out the qr wrt goal minus the 
    # location of the qr code wrt robot
-    goal_wrt_map_x = .5 #sample
-    goal_wrt_map_y = .5 #sample
-    orientation = 1 # figure out orientation wrt map frame
+    position_x = data.pose.position.x
+    position_y = data.pose.position.y
+    orientation = data.pose.orientation.w
+
+    goal_wrt_map_x = .5 - position_x #sample
+    goal_wrt_map_y = .5 - position_y #sample
+    orientation = 1 - orientation # figure out orientation wrt map frame
 
    # Create an action client called "move_base" with action definition file "MoveBaseAction"
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
@@ -27,7 +35,7 @@ def movebase_client():
 
    # Creates a new goal with the MoveBaseGoal constructor
     goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "map" #I guess we can change the target frame.
+    goal.target_pose.header.frame_id = "camera_odom_frame" 
     goal.target_pose.header.stamp = rospy.Time.now()
    # Move 0.5 meters forward along the x axis of the "map" coordinate frame 
     goal.target_pose.pose.position.x = goal_wrt_map_x
@@ -43,20 +51,33 @@ def movebase_client():
    # If the result doesn't arrive, assume the Server is not available
     if not wait:
         rospy.logerr("Action server not available! Switch to manual control")
-        rospy.signal_shutdown("Action server not available!")
+        rospy.signal_shutdown("Action server not available! Switch to manual control")
         # At this point manual override should commence. So should add that here.
     else:
     # Result of executing the action
+        rospy.set_param('nav_to_mine', 1) #Trigger for mining script
         return client.get_result()   
-        # Call the mining script
-        # This could be done by publishing a sort of "completed task" signal which the 
-        # mining script would depend upon and start when it publishes 1.
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber("/object_22", PoseStamped, movebase_client)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
 # If the python node is executed as main process (sourced directly)
 if __name__ == '__main__':
     try:
        # Initializes a rospy node to let the SimpleActionClient publish and subscribe
         rospy.init_node('movebase_client_py')
+        listener()
         result = movebase_client()
         if result:
             rospy.loginfo("Goal execution done!")
